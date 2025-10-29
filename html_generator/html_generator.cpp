d051b245af063edd33d41b4ffe9d0f11177b340c
home <chmpgnespnv@khu.ac.kr>
2025-10-29 10:27:42 +0900

    feature/ html 이름 변경 latest_messages.html -> index.html

--- html_generator/html_generator.cpp
+++ html_generator/html_generator.cpp
@@ -1,6 +1,6 @@
 #include "json_to_html.h"
 
 int main() {
-    convert_json_to_html("../data/latest_messages.json", "../data/latest_messages.html");
+    convert_json_to_html("../data/latest_messages.json", "../data/index.html");
     return 0;
 }
